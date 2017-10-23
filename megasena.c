87b3da562db755d3d524cc87946207d0833728f9
Carolina Alves <33011285+carolinalvess@users.noreply.github.com>
2017-10-22 22:17:51 -0200

    Create megasena.c

---
+++ megasena.c
@@ -0,0 +1,55 @@
+# include  < stdio.h >
+int  main () {
+
+int i, j , w = 0 ;
+int n [ 6 ]; 
+int x [ 6 ]; 
+
+para (i = 0 ; i < 6 ; i ++) {
+
+   printf ( " insira os numeros sorteados: " );
+   scanf ( " % d " , & x [i]);
+   }
+para (i = 0 ; i < 6 ; i ++) {
+
+   printf ( "\ninsira os numeros que jogou: " );
+   scanf ( " % d " , & n [i]);
+
+   }
+
+para (i = 0 ; i < 6 ; i ++) {
+
+   printf ( " % d \ t % d \ n " , x [i], n [i]);
+   }
+
+para (i = 0 ; i < 6 ; i ++) {
+para (j = 0 ; j < 6 ; j ++) {
+   se (n [i] == x [j]) {
+       w ++;
+   }
+   }
+   }
+mudar (w) {
+   caso  0 :
+   printf ("Não foi hoje");
+   quebrar;
+   caso  1:
+   printf ("Não foi hoje");
+   quebrar;
+   caso  2:
+   printf ("Não foi hoje");
+   quebrar;
+   caso  3:
+   printf ("Quase");
+   quebrar;
+   caso  4:
+   printf ("Ganhou quadra");
+   quebrar;
+   caso  5:
+   printf ("Ganhou quina");
+   quebrar;
+   caso  6:
+   printf ("Ganhou tudo");
+   quebrar;
+   }
+   }
